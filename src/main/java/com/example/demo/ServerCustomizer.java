53c2a430195a160dd7e83b96b3aac2b804fbe2bf
adirremi <89388145+adirremi@users.noreply.github.com>
2022-07-27 14:41:59 +0300

    Delete ServerCustomizer.java

--- src/main/java/com/example/demo/ServerCustomizer.java
+++
@@ -1,9 +0,0 @@
-@Component  
-public class ServerCustomizer implements WebServerFactoryCustomizer< ConfigurableWebServerFactory <   
-{  
-@Override  
-public void customize(ConfigurableWebServerFactory factory)   
-{  
-factory.setPort(9001);  
-}  
-}  
