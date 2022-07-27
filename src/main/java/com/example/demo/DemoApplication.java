1380d99ce8f1da5964c9b57440fc35e5ad230c2c
adirremi <89388145+adirremi@users.noreply.github.com>
2022-07-27 14:16:51 +0300

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -20,12 +20,3 @@ public class DemoApplication {
 	}
 
 }
-@Component  
-public class ServerCustomizer implements EmbeddedServletContainerCustomizer   
-{  
-@Override  
-public void customize(ConfigurableEmbeddedServletContainer container)   
-{  
-container.setPort(8097);  
-}  
-}  
