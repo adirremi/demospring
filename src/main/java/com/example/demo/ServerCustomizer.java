9dc419e79e4cae0b3c853df9fa687e1796ffe46f
adirremi <89388145+adirremi@users.noreply.github.com>
2022-07-27 14:23:00 +0300

    Delete ServerCustomizer.java

--- src/main/java/com/example/demo/ServerCustomizer.java
+++
@@ -1,9 +0,0 @@
-@Component  
-public class ServerCustomizer implements EmbeddedServletContainerCustomizer   
-{  
-@Override  
-public void customize(ConfigurableEmbeddedServletContainer container)   
-{  
-container.setPort(8181);  
-}  
-}  
