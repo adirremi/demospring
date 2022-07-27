39422d7ed5f19a593f948faf4c6fea8edeeadeba
adirremi <89388145+adirremi@users.noreply.github.com>
2022-07-27 14:36:22 +0300

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,10 +15,7 @@ public class DemoApplication {
 	}
 
 	public static void main(String[] args) {
-	SpringApplication app = new SpringApplication(CustomApplication.class);
-        app.setDefaultProperties(Collections
-          .singletonMap("server.port", "8083"));
-        app.run(args);
+	SpringApplication.run(DemoApplication.class, args);
 		
 	}
 
